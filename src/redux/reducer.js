099854c641e39e748c4a8d65cfb822b11821c27a
Gogolbog <111005996+Gogolbog@users.noreply.github.com>
2023-05-14 22:58:19 +0300

    deleted: reducer that dont used

--- src/redux/reducer.js
+++
@@ -1,8 +0,0 @@
-// import { combineReducers } from '@reduxjs/toolkit';
-// import { contactsSlice } from './contacts/contactsSlice';
-// import { filterSlice } from './filter/filterSlice';
-
-// export const reducer = combineReducers({
-//   contacts: contactsSlice.reducer,
-//   filter: filterSlice.reducer,
-// });
